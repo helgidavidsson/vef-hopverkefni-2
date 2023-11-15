cd50e015675f9bd08bdbf2b8c5f3ecfb9081a382
helgidavidsson <helgifreyr02@gmail.com>
2023-11-15 07:39:29 +0000

    intalized

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export default function Header() {
+    return(
+        <div className={styles.header}>
+            <h1 className={styles.h1}>Vefforritunarbúðin</h1>
+        </div>
+    )
+}
